421d79d80d7bfff092e9927ce64270b645585185
Luis <luis.a110686@gmail.com>
2024-05-17 21:14:25 +0100

    svg elements updated (luis)

--- SVGElements.cpp
+++ SVGElements.cpp
@@ -11,27 +11,28 @@ namespace svg
         : fill(fill), center(center), radius(radius)
     {
     }
-    Point Ellipse::transform(const std::string &t_name, const Point &origin, int mul = 0) const
+    Point Ellipse::transform(const std::string &t_name, const Point &origin, int &mul = NULL) const
         if(t_name == "translate"){
             return center.translate(origin);
         }
-    Point Ellipse::rotate(const std::string &t_name, const Point &origin, int radius) const{
-        if(t_name == "rotate"){
-            return center.rotate(origin);
-    }
-    Point Ellipse::scale(const std::string &t_name, const Point &origin, int mul = 0, const int &val){
-        if(t_name == "scale"){
-            return center.scale(origin);
+    
+        else if(t_name == "rotate"){
+            return center.rotate(origin, mul);
+    
+        }
+        else if(t_name == "scale"){
+            return center.scale(origin, mul);
         }
     }
 
-    void Ellipse::draw(PNGImage &img, transform) const
+    void Ellipse::draw(PNGImage &img) const
     {
+        img.draw_ellipse(center,radius,fill);
     }
 
 
 
-    }
+
 
 
 
@@ -42,26 +43,95 @@ namespace svg
     {
     }
 
+
+
+    std::vector<Point> Polyline::transform(const std::string &t_name, const std::vector<Point>& points, int &mul = NULL) const
+
+   
+    if(t_name == "translate"){
+        std::vector<Point> new_points;
+        for(auto p : points){
+            new_points.push_back(p.translate(origin));
+            }
+    return new_points;
+
+    else if (t_name == "rotate"){
+        std::vector<Point> new_points;
+        for(auto p : points){
+            new_points.push_back(p.rotate(origin, mul));
+            
+            } 
+        return new_points; 
+
+    else if(t_name == "scale"){
+        std::vector<Point> new_points;
+        for(auto p : points){
+            new_points.push_back(p.scale(origin, mul));
+            }
+        return new_points;
+    }
+    }
+
+
     void PolyLine::draw(PNGImage &img) const
     {
-        img.draw_PolyLine(, stroke);
+        for(int i = 0; i< points.size() - 1; i++)
+            img.draw_line(points.at(i), points.at(i+1), stroke);
     }
+
+
+
+
+
+
 
     Polygon::Polygon(const Color &fill, const std::vector<Point>& points)
         : fill(fill), points(points)
     {
     }
 
-    void Polygon::draw(PNGImage &img) const
-    {
-        img.draw_Polygon(points, fill);
+    std::vector<Point> Polygon::transform(const std::string &t_name, const std::vector<Point>& points, int mul = 0) const{
+    if(t_name == "translate"){
+        std::vector<Point> new_points;
+        
+        
+        for(auto p : points){
+            new_points.push_back(p.translate(origin));
+        }
+        return new_points;
     }
 
-    Point Polygon::transform(const std::string &t_name, const std::vector<Point>& points, int mul = 0) const
-    if(t_name == "translate"){
-        
+
+        else if(t_name == "rotate"){
+        std::vector<Point> new_points;
+        for(auto p : points){
+            new_points.push_back(p.rotate(origin));
+        }
+        return new_points;
+    }
+
+
+        else if(t_name == "scale"){
+        std::vector<Point> new_points;
+        for(auto p : points){
+            new_points.push_back(p.scale(origin));
+        }
+        return new_points;
+    }
+    }
+
+
+        void Polygon::draw(PNGImage &img) const
+    {
+        if(new_points == NULL){
+            img.draw_polygon(points, fill);
+        }
+        else{
+            img.draw_polygon(new_points, fill);
+        }
+    }
 
 
 
 }
-}
+
